85c7aea459968c9cc08dbb443078711594dfaa74
Calebbii <biicaleb316@gmail.com>
2021-10-13 16:59:43 +0300

    updates the class citation

--- src/app/citation.ts
+++ src/app/citation.ts
@@ -1,3 +1,3 @@
 export class Citation {
-    constructor(public id:number, public citeName:string, public citeEmail:string, public citeMessage:string, public upVote:number, public  downVote:number){}
+    constructor(public id:number, public citeName:string, public citeEmail:string, public citeMessage:string, public upVote:number, public  downVote:number, public datePosted:Date){}
 }
